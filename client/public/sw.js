a7229d0c425f4bf678172bd417906fc9c6ef4bcf
Redwan <redwanulislam12@gmail.com>
2019-11-02 12:48:22 +0600

    sdfwe

--- client/public/sw.js
+++ client/public/sw.js
@@ -16,7 +16,7 @@ self.addEventListener("push", e => {
 
 self.addEventListener('activate', async (event) => {
     
-    event.waitUntil(clients.claim());
+    //event.waitUntil(clients.claim());
     // This will be called only once when the service worker is activated.
     console.log('service worker activate')
 })
